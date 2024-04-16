5eb81b4e41e6f0f8a5d1518369171e90cbe21e8a
Name Surname <email@email.com>
2024-04-16 20:55:41 +0300

    DOM 16.04.2024

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,9 @@
 // TODO: write code here
+
+import PlayingArea from './playing-area';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const playingArea = new PlayingArea(document.querySelector('.playing-area'));
+
+  setInterval(playingArea.randomMovingGoblin, 1000);
+})
